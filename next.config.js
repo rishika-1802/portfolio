8684d25e95d878b5fc94380b25095f74dd86927b
Rishika B S <rishika1826@gmail.com>
2026-09-21 22:54:04 +0530

    Configure portfolio for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [],
   },
 }
